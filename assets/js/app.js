218f41355a7f2a60d0bebc429bb7c7625f574091
CarlDesigner <krlitosfajardo626@gmail.com>
2022-10-16 12:51:15 -0500

    Se crea la logica javascrip dejando la aplicación funcional

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const salida = document.getElementById('generarContrasena')
+document.getElementById('copiar').addEventListener('click', copiar)
+
+function generarContrasena(event) {
+    const contador = 10,
+        caracteres =
+            'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ,;:!¡_-¿?#$%&/=*[]@{}'
+
+    var gdc = ''
+    for (var i = 0, n = caracteres.length; i < contador; ++i) {
+        gdc += caracteres.charAt(Math.floor(Math.random() * n))
+    }
+
+    salida.innerHTML = gdc
+}
+
+function copiar() {
+    const copyText = salida.innerHTML
+    navigator.clipboard.writeText(copyText).then(() => {
+        alert('Copied to Clipboard : ' + copyText)
+    })
+};
